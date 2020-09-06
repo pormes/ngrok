f6e59ea2aa427f46eb35d28320dd3d4670b9a68a
Pormes <66345108+pormes@users.noreply.github.com>
2020-09-07 02:42:09 +0700

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -3,9 +3,13 @@ echo "Activasi Ngrok"
 pkg install -y
 pkg update -y
 pkg upgrade -y
+pkg install golang -y
+pkg install automake -y
 pkg install wget -y
 pkg install zip -y
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
 unzip ngrok-stable-linux-arm.zip
 
 echo "SILAHKAN PAKAI TOKEN AUTH ANDA SENDIRI"
+echo "CODED BY PORMES"
+echo "SUBSCRIBE PORMES17 ON YOUTUBE"

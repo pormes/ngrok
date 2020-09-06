2e61af77ae501276421dfd32d8e706e882534f11
Pormes <66345108+pormes@users.noreply.github.com>
2020-09-07 02:20:06 +0700

    Create ngrok.sh

---
+++ ngrok.sh
@@ -0,0 +1,11 @@
+echo "Activasi Ngrok"
+
+pkg install -y
+pkg update -y
+pkg upgrade -y
+pkg install wget -y
+pkg install zip -y
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+unzip ngrok-stable-linux-arm.zip
+
+echo "SILAHKAN PAKAI TOKEN AUTH ANDA SENDIRI"

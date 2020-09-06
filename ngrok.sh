58d13f6219bb0a012ee921a1c717082b103e7d6b
Pormes <66345108+pormes@users.noreply.github.com>
2020-09-07 02:59:31 +0700

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -7,9 +7,16 @@ pkg install golang -y
 pkg install automake -y
 pkg install wget -y
 pkg install zip -y
+
+echo "PROSES DOWNLOAD <===>"
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+echo "PROSES UNZIP FILE"
 unzip ngrok-stable-linux-arm.zip
 
 echo "SILAHKAN PAKAI TOKEN AUTH ANDA SENDIRI"
+echo "AKTIFKAN HOTSPOT ANDA"
+echo "$ ./ngrok http 80"
+echo "$ NEW TAB/NEW SESION"
+echo "$ make"
 echo "CODED BY PORMES"
 echo "SUBSCRIBE PORMES17 ON YOUTUBE"
